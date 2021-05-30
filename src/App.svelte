1c2c2159cce1cfdfdf383aa44bd95902cc901370
James Camilleri <james@james.com.mt>
2021-05-30 20:14:07 +0200

    Add logo, initial circle generator code

--- src/App.svelte
+++ src/App.svelte
@@ -6,18 +6,53 @@ Level 3 Centris Business Gateway,
 Triq is-Salib ta' l-Imriehel, Zone 3
 Central Business District CBD 3020 – Malta`
 
+  const MAX_X = 100;
+  const MAX_Y = 150;
+
   let name = 'Sarah Camilleri';
   let designation = 'Data Analyst';
   let mobileNumber;
   let landlineNumber = '(+356) 21312020';
+
+  let circleA
+  let circleB
+
+  $: circleA = {
+    cx: Math.abs(hash(name.split(' ')[0])) / 2147483647 * MAX_X,
+    cy: Math.abs(hash(name.split(' ')[0].split('').reverse().toString())) / 2147483647 * MAX_Y,
+    r: name.split(' ')[0].length * 2
+  }
+
+  $: circleB = {
+    cx: Math.abs(hash(name.split(' ')[1])) / 2147483647 * MAX_X,
+    cy: Math.abs(hash(name.split(' ')[1].split('').reverse().toString())) / 2147483647 * MAX_Y,
+    r: name.split(' ')[1].length * 3
+  }
 
   const styles = {
     global: 'font-family: Trebuchet MS, sans-serif;',
     name: 'font-size: 20px; font-weight: bold;',
     designation: 'font-size: 16px; font-weight: bold; margin-bottom: 10px;',
     info: 'font-size: 14px;',
-    address: 'margin-top: 10px;'
+    address: 'margin-top: 10px;',
+    logo: 'margin-top: 10px;'
   }
+
+  function hash (string) {
+      if (!string) return 0
+
+	    let hash = 0
+
+      if (string.length === 0) return hash
+
+      for (let i = 0; i < string.length; i++) {
+	        const char = string.charCodeAt(i)
+	        hash = ((hash << 5) - hash) + char
+	        hash = hash & hash // Convert to 32bit integer
+	    }
+
+	    return hash
+	}
 </script>
 
 <main>
@@ -27,17 +62,25 @@ Central Business District CBD 3020 – Malta`
       <div style={styles.designation}>{designation}</div>
     </div>
     <table>
-      <td></td>
-      <td style={styles.info}>
-        {#if mobileNumber}<div>{mobileNumber}</div>{/if}
-        <div>{landlineNumber}</div>
-        <div style={styles.address}>
-          {#each address.split(/\r?\n/) as line}
-            <div>{line}</div>
-          {/each}
-        </div>
-      </td>
+      <tr>
+        <td>
+          <svg width={MAX_X} height={MAX_Y}>
+            <circle {...circleA} fill="blue" />
+            <circle cx={circleB.cx} cy={circleB.cy} r="5" fill="blue" />
+          </svg>
+        </td>
+        <td style={styles.info}>
+          {#if mobileNumber}<div>{mobileNumber}</div>{/if}
+          <div>{landlineNumber}</div>
+          <div style={styles.address}>
+            {#each address.split(/\r?\n/) as line}
+              <div>{line}</div>
+            {/each}
+          </div>
+        </td>
+      </tr>
     </table>
+    <img src="/logo.png" alt="Risk Management logo" style={styles.logo}>
   </div>
   <div>
       <div class="form-control">
@@ -60,6 +103,9 @@ Central Business District CBD 3020 – Malta`
         <label for="address">Address</label>
         <textarea id="address">{address}</textarea>
       </div>
+
+      <div>{circleA.cx}</div>
+      <div>{circleA.cy}</div>
   </div>
 </main>
 
